3b8eacfda506508e0843f58dbf6d0028b4165ef7
hyperboleX <hyperboloide@gmail.com>
2026-08-05 09:45:38 -0400

    Task 2

---
+++ emotion_detection.py
@@ -0,0 +1,9 @@
+import requests
+
+def emotion_detector (text_to_analyze):
+    URL: 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    headers: {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    inputobj: { "raw_document": { "text": text_to_analyze } }
+
+    response = requests.post(url, json = inputobj, headers=headers)
+    return response.text
