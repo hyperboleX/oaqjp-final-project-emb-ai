90c30175981c0afaae0817a4d3d5b540bc8c65f0
hyperboleX <hyperboloide@gmail.com>
2026-08-05 20:08:05 -0400

    Task 8

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -1,6 +1,8 @@
 import requests, json
 
 def emotion_detector (text_to_analyze):
+    """ Emotion detector
+    """
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     inputobj = { "raw_document": { "text": text_to_analyze } }
--- server.py
+++ server.py
@@ -1,3 +1,6 @@
+""" Main DocString
+"""
+
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,22 +8,25 @@ app = Flask("emotion_detector")
 
 @app.route("/emotionDetector")
 def sent_emotion():
+    """server function
+    """
     text_to_analyze = request.args.get('textToAnalyze')
     response = emotion_detector(text_to_analyze)
-    
     if response is None:
         return "Invalid text! Please try again!"
-    else:
-        emotions = ", ".join(f"{k}: {v}" for k, v in list(response.items())[:-2])
-        emotion_last = ", ".join(f"{k}: {v}" for k, v in list(response.items())[-2:-1])
-        last_key, last_val = list(response.items())[-1]
-        dominant = f"{last_key} is {last_val}"
-        final_str = "For the given statement, the system response is {} and {}. The {}.".format(emotions, emotion_last, dominant)
-        return final_str
-        
+    emotions = ", ".join(f"{k}: {v}" for k, v in list(response.items())[:-2])
+    emotion_last = ", ".join(f"{k}: {v}" for k, v in list(response.items())[-2:-1])
+    last_key, last_val = list(response.items())[-1]
+    final_str1 = f"For the given statement, the system response is {emotions}"
+    final_str2 = f" and {emotion_last}."
+    dominant = f" The {last_key} is {last_val}"
+    final_str = final_str1 + final_str2 + dominant
+    return final_str
 @app.route("/")
 def render_index_page():
-	return render_template('index.html')
+    """ Access to template
+    """
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
